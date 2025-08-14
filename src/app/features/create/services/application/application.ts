94e89b22f6aebb22f956db9d1198884b12f27957
tirlipinpon <tirlipinpon@hotmail.com>
2025-08-14 13:29:02 +0200

    ajout fonction appel de application vers store method

--- src/app/features/create/services/application/application.ts
+++ src/app/features/create/services/application/application.ts
@@ -11,4 +11,40 @@ export class Application {
   getNextPostId(): void {
     this.store.getNextPostId();
   }
+
+  setTitre(): void {
+    this.store.setTitre();
+  }
+
+  setDescriptionMeteo(): void {
+    this.store.setDescriptionMeteo();
+  }
+
+  setPhraseAccroche(): void {
+    this.store.setPhraseAccroche();
+  }
+
+  setArticle(): void {
+    this.store.setArticle();
+  }
+
+  setNewHref(): void {
+    this.store.setNewHref();
+  }
+
+  setCitation(): void {
+    this.store.setCitation();
+  }
+
+  setLienUrlArticle(): void {
+    this.store.setLienUrlArticle();
+  }
+
+  setImageUrl(): void {
+    this.store.setImageUrl();
+  }
+
+  setCategorie(): void {
+    this.store.setCategorie();
+  }
 }
